6b514c0f87ec5f2b180349133f2d7c32e2966c2a
greendb16 <greendb16@gmail.com>
2019-06-12 13:47:23 -0400

    Final Final Commit

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class InputOutput {
     public static void main(String[]args){
         Scanner keyboard = new Scanner(System.in);
-        int num1, num2, num3;
+        double num1, num2, num3;
 
         System.out.print( "First Temperature?");
-        num1 = keyboard.nextInt();
+        num1 = keyboard.nextDouble();
 
         System.out.print( "Second Temperature?");
-        num2 = keyboard.nextInt();
+        num2 = keyboard.nextDouble();
 
-        System.out.println( "The average value is : " + ((num1 + num2)/2));
+        num3 = (num1 + num2)/2;
+        System.out.println( "The average value is : " + num3);
     }
 }
